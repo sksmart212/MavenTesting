20506e44a6e9e73b0719035e4f4ad9868bd5d5e8
Gupta <Sushank.Gupta1@kantar.com>
2020-05-09 17:36:57 +0100

    new

---
+++ src/main/java/AppTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AppTest {
+@Test
+    public void main()
+
+    {
+      // System.out.println("Testing Completed");
+        System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
+        ChromeDriver driver=new ChromeDriver();
+        driver.navigate().to("http://localhost:9090/KPS/");
+    }
+}
